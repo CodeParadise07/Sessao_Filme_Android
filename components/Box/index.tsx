057597e04fd0589fd332e12ea2eb1fd7fc29ac9d
Sávio Soares <dsaviosnogueira@gmail.com>
2023-02-18 23:27:30 -0300

    Create scroll horizontal at home

--- components/Box/index.tsx
+++ components/Box/index.tsx
@@ -1,5 +1,36 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-export function Box(text: String) {
-  return <Text>{text}</Text>;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#1F1F1F",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    maxWidth: 300,
+    maxHeight: 300,
+    paddingHorizontal: 30,
+    textAlign: "center",
+  },
+  title: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 32,
+  },
+  overview: {
+    color: "#fff",
+  },
+  vote_average: {
+    color: "#9332D6",
+  },
+});
+
+export function Box(data: any) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{data.data.title}</Text>
+      <Text style={styles.overview}>{data.data.overview}</Text>
+      <Text style={styles.vote_average}>{data.data.vote_average}</Text>
+    </View>
+  );
 }
--- pages/Home/home.tsx
+++ pages/Home/home.tsx
@@ -1,27 +1,45 @@
-import { Text, StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import { useState, useEffect } from "react";
+import { Box } from "../../components/Box";
 
 export function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=dfe68602b34df2dbb9c6c15dcfea7dfe&language=pt-BR&page=1`
+    )
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results));
+  }, []);
+
   const style = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
       backgroundColor: "#1f1f1f",
+      color: "#fff",
     },
     title: {
       color: "#fff",
       fontSize: 24,
-    },
-    icon: {
-      height: 30,
-      width: 30,
-      backgroundColor: "#fff",
+      padding: 10,
+      fontStyle: "bold",
     },
   });
 
   return (
     <View style={style.container}>
-      <View></View>
+      <View>
+        <Text style={style.title}>Populares</Text>
+        <FlatList
+          horizontal={true}
+          data={movies}
+          ListEmptyComponent={() => <Text>Ninguem chegou</Text>}
+          renderItem={({ item }) => {
+            return <Box data={item} />;
+          }}
+        ></FlatList>
+      </View>
     </View>
   );
 }
